3396b41e62a85b6bc2dd588e661908222a1a6b7e
benmalartre <benmalartre@hotmail.com>
2020-02-23 05:12:34 +0100

    initial commit with basic CMakeLists.txt

---
+++ build/src/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/hello_world.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/hello.dir/hello_world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/benmalartre/Documents/RnD/USD_BUILD/include"
+  "/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
