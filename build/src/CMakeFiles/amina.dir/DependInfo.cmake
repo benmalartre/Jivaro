af8c1a7f2710701c28a0807da8c9d14c03d9c816
benmalartre <benmalartre@hotmail.com>
2020-02-29 02:23:25 +0100

    Render result in the viewport

---
+++ build/src/CMakeFiles/amina.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/gl3w/src/gl3w.c" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/gl3w/src/gl3w.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "IMGUI_IMPL_OPENGL_LOADER_GL3W"
+  "TASKING_TBB"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  "/Users/benmalartre/Documents/RnD/USD_BUILD/include"
+  "/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  "/Users/benmalartre/Documents/RnD/embree/embree/include"
+  "/Users/benmalartre/Documents/RnD/embree/embree/common"
+  "/Users/benmalartre/Documents/RnD/embree/embree/tutorials/common"
+  "/Users/benmalartre/Documents/RnD/glfw/include"
+  "../src/gl3w/include"
+  "/Users/benmalartre/Documents/RnD/USD_BUILD/include/tbb"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/application.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/application.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/context.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/context.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/device.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/device.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui_demo.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui_demo.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui_draw.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui_draw.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui_impl_glfw.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui_impl_glfw.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui_impl_opengl3.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui_impl_opengl3.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui_test.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui_test.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/imgui/imgui_widgets.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/imgui/imgui_widgets.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/jpeg.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/jpeg.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/main.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/main.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/mesh.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/mesh.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/oiio.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/oiio.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/splitter.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/splitter.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/ui.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/ui.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/utils.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/utils.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/view.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/view.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/widgets/dummy.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/widgets/dummy.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/widgets/viewport.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/widgets/viewport.cpp.o"
+  "/Users/benmalartre/Documents/RnD/embree/embree-usd/src/window.cpp" "/Users/benmalartre/Documents/RnD/embree/embree-usd/build/src/CMakeFiles/amina.dir/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMGUI_IMPL_OPENGL_LOADER_GL3W"
+  "TASKING_TBB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  "/Users/benmalartre/Documents/RnD/USD_BUILD/include"
+  "/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7"
+  "/Users/benmalartre/Documents/RnD/embree/embree/include"
+  "/Users/benmalartre/Documents/RnD/embree/embree/common"
+  "/Users/benmalartre/Documents/RnD/embree/embree/tutorials/common"
+  "/Users/benmalartre/Documents/RnD/glfw/include"
+  "../src/gl3w/include"
+  "/Users/benmalartre/Documents/RnD/USD_BUILD/include/tbb"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/amina.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/amina.dir/application.cpp.o"
+  "CMakeFiles/amina.dir/context.cpp.o"
+  "CMakeFiles/amina.dir/device.cpp.o"
+  "CMakeFiles/amina.dir/gl3w/src/gl3w.c.o"
+  "CMakeFiles/amina.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/amina.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/amina.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/amina.dir/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/amina.dir/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/amina.dir/imgui/imgui_test.cpp.o"
+  "CMakeFiles/amina.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/amina.dir/jpeg.cpp.o"
+  "CMakeFiles/amina.dir/main.cpp.o"
+  "CMakeFiles/amina.dir/mesh.cpp.o"
+  "CMakeFiles/amina.dir/oiio.cpp.o"
+  "CMakeFiles/amina.dir/splitter.cpp.o"
+  "CMakeFiles/amina.dir/ui.cpp.o"
+  "CMakeFiles/amina.dir/utils.cpp.o"
+  "CMakeFiles/amina.dir/view.cpp.o"
+  "CMakeFiles/amina.dir/widgets/dummy.cpp.o"
+  "CMakeFiles/amina.dir/widgets/viewport.cpp.o"
+  "CMakeFiles/amina.dir/window.cpp.o"
+  "amina"
+  "amina.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/amina.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
